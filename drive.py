f086d73027e65ccbd5b11f00c66e94d34529374a
Ninad Ghike <Ninad Ghike>
2017-04-21 18:03:44 -0700

    updating as per review instructions

--- drive.py
+++ drive.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import os
 import shutil
 
+import cv2
+
 import numpy as np
 import socketio
 import eventlet
@@ -21,6 +23,12 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
+
+def i_crop(I):
+    return I[50:,:]
+
+def i_resize(I):
+    return cv2.resize(I,(64, 64),interpolation=cv2.INTER_AREA)
 
 class SimplePIController:
     def __init__(self, Kp, Ki):
@@ -61,6 +69,9 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = i_crop(image_array)
+        image_array = i_resize(image_array)
+        ##
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
